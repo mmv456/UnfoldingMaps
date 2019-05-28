1146d2e06066b33cb26e5e0ad9aeb4585f922839
Mahitha Valluru <mahitha.valluru@gmail.com>
2019-05-27 21:25:27 -0400

    Added events to interact with markers

---
+++ Earthquake_UserActions/CityMarker.java
@@ -0,0 +1,98 @@
+package Earthquake_UserActions;
+
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
+import processing.core.PConstants;
+import processing.core.PGraphics;
+
+/** Implements a visual marker for cities on an earthquake map
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * @author Your name here
+ *
+ */
+// TODO: Change SimplePointMarker to CommonMarker as the very first thing you do 
+// in module 5 (i.e. CityMarker extends CommonMarker).  It will cause an error.
+// That's what's expected.
+public class CityMarker extends CommonMarker {
+	
+	public static int TRI_SIZE = 5;  // The size of the triangle marker
+	
+	public CityMarker(Location location) {
+		super(location);
+	}
+	
+	
+	public CityMarker(Feature city) {
+		super(((PointFeature)city).getLocation(), city.getProperties());
+		// Cities have properties: "name" (city name), "country" (country name)
+		// and "population" (population, in millions)
+	}
+
+	
+	/**
+	 * Implementation of method to draw marker on the map.
+	 */
+	public void drawMarker(PGraphics pg, float x, float y) {
+		// Save previous drawing style
+		pg.pushStyle();
+		
+		// IMPLEMENT: drawing triangle for each city
+		pg.fill(150, 30, 30);
+		pg.triangle(x, y-TRI_SIZE, x-TRI_SIZE, y+TRI_SIZE, x+TRI_SIZE, y+TRI_SIZE);
+		
+		// Restore previous drawing style
+		pg.popStyle();
+	}
+	
+	/** Show the title of the city if this marker is selected */
+	public void showTitle(PGraphics pg, float x, float y)
+	{
+		
+		// TODO: Implement this method
+		int x_base = (int) x-10;
+		int y_base = (int) y+25;
+		
+		String topText = this.getCity() + ", " + this.getCountry();
+		String population = "Population: " + (int) (this.getPopulation() * 10) + " million";
+		
+		int textLength;
+		
+		if(topText.length() > population.length()) {
+			textLength = topText.length();
+		}
+		else {
+			textLength = population.length();
+		}
+		
+		
+		pg.fill(150, 150, 150); // rectangle
+		pg.rect(x-15,  y+10, (textLength * 5) + (textLength + 15), 35);
+		
+		pg.fill(0, 0, 0); // text
+		pg.text(topText, x_base, y_base);
+		pg.text(population, x_base, y_base + 15);
+		
+	}
+	
+	
+	
+	/* Local getters for some city properties.  
+	 */
+	public String getCity()
+	{
+		return getStringProperty("name");
+	}
+	
+	public String getCountry()
+	{
+		return getStringProperty("country");
+	}
+	
+	public float getPopulation()
+	{
+		return Float.parseFloat(getStringProperty("population"));
+	}
+}
